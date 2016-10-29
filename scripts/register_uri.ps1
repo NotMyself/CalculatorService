571959986978154ebe61ac45a63329699ce41328
Bobby Johnson <bobby.johnson@gmail.com>
2016-10-29 12:55:00 -0700

    clarifying username parameter for register_uri.ps1

--- scripts/register_uri.ps1
+++ scripts/register_uri.ps1
@@ -1,3 +1,3 @@
-$userName = Read-Host -Prompt 'Input User Name' 
+$userName = Read-Host -Prompt 'Input User Name (notmyself for local OR domain\notmyself for AD)' 
  
  netsh http add urlacl url=http://+:8080/calc user="$userName"
